4f0b94837256aee8ae32c3b6ec39c5ea1451e7aa
NBaker <nbaker@herculesrx.com>
2021-04-05 16:20:34 +0800

    Added movie recommendation project

--- js/main.js
+++ js/main.js
@@ -154,5 +154,47 @@
 		sliderMain();
 	});
 
+	// Porfolio isotope and filter
+	$(window).on('load', function() {
+		var portfolioIsotope = $('.portfolio-container').isotope({
+		  itemSelector: '.portfolio-item'
+		});
+	
+		$('#portfolio-flters li').on('click', function() {
+		  $("#portfolio-flters li").removeClass('filter-active');
+		  $(this).addClass('filter-active');
+	
+		  portfolioIsotope.isotope({
+			filter: $(this).data('filter')
+		  });
+		  aos_init();
+		});
+	
+		// Initiate venobox (lightbox feature used in portofilo)
+		$('.venobox').venobox({
+		  'share': false
+		});
+	
+		// Initiate aos_init() function
+		aos_init();
+	
+	  });
+	
+	  // Testimonials carousel (uses the Owl Carousel library)
+	  $(".testimonials-carousel").owlCarousel({
+		autoplay: true,
+		dots: true,
+		loop: true,
+		items: 1
+	  });
+	
+	  // Portfolio details carousel
+	  $(".portfolio-details-carousel").owlCarousel({
+		autoplay: true,
+		dots: true,
+		loop: true,
+		items: 1
+	  });
+
 
 }());
